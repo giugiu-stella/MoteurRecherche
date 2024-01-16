ff8e5c9d5885d84561756f3680663c2c42526baa
Kira13s <kira13sangmele@protonmail.com>
2024-01-16 14:57:37 +0100

    Betweenness Centrality

--- backend/server/graph.py
+++ backend/server/graph.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 class Node:
     def __init__(self, json):
@@ -20,6 +21,49 @@ class Graph:
         node = Node(value)
         self.nodes.append(node)
         return node
+    
+    def calculate_betweenness_centrality(self):
+        C_B = [0 for _ in range(len(self.nodes))]
+        for s in self.nodes:
+            S = deque()
+            P = dict()
+            sigma = dict()
+            d = dict()
+            
+            for w in self.nodes:
+                if w == s:
+                    sigma[w] = 1
+                    d[w] = 0
+                else:
+                    sigma[w] = 0
+                    d[w] = -1
+                P[w] = []
+            
+            Q = deque()
+            Q.append(s)
+            
+            while len(Q) != 0:
+                v = Q.popleft()
+                S.append(v)
+                
+                for w in v.neighbors:
+                    if d[w] < 0:
+                        Q.append(w)
+                        d[w] = d[v] + 1
+
+                if d[w] == d[v] + 1:
+                    sigma[w] = sigma[w] + sigma[v]
+                    P[w].append(v)
+            
+            delta = {v : 0 for v in self.nodes}
+            while len(S) != 0:
+                w = S.popleft()
+                for v in P[w]:
+                    delta[v] += sigma[v] / sigma[w] * (1 + delta[w])
+                if w != s:
+                    C_B[w] += delta[w]
+                    
+        self.nodes.sort(key=lambda x: C_B[x])    
     
     def sort_nodes_by_centrality_measure(self):
         self.nodes.sort(key=lambda x: x.centrality_measure)
